9b6a46351db5bc8dc226e80722d18cc313e0bb33
Kaelinator <kirkkael@gmail.com>
2019-10-18 16:59:12 -0500

    getting user input

--- QueensAreSafe/src/com/kaelkirk/QueensAreSafe.java
+++ QueensAreSafe/src/com/kaelkirk/QueensAreSafe.java
@@ -1,9 +1,29 @@
 package com.kaelkirk;
 
-import java.util.Arrays;
-
 public class QueensAreSafe {
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(args));
+  public static void main(String[] args) {
+
+    int n = parseN(args);
+    System.out.printf("You chose %dx%d board.", n, n);
+  }
+
+  private static int parseN(String[] args) {
+    if (args.length != 1)
+      exitWithUsage();
+
+    int n = -1;
+    try {
+      n = Integer.parseUnsignedInt(args[0]); // >= 0
+    } catch (Exception e) {
+      System.err.println("N must be an integer >= 0.");
+      exitWithUsage();
     }
+
+    return n;
+  }
+
+  public static void exitWithUsage() {
+    System.out.println("Usage: queensaresafe N");
+    System.exit(1);
+  }
 }
